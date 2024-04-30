d961a1be2cafcf0dee2d32d6403ce9422ff9192e
Dmitry Kalita <rusmonster@gmail.com>
2024-04-30 12:29:53 +0300

    build: Fix fetching artifacts

--- tools/rtd-sdk-cdn-pin/fetch/common/fetch-sonatype-android.sh
+++ tools/rtd-sdk-cdn-pin/fetch/common/fetch-sonatype-android.sh
@@ -9,14 +9,16 @@ function fetchFromSonatype() {
 
   if [[ ("${SONATYPE_RELEASE_VERSION}" =~ ^[0-9]+\.[0-9]+\.[0-9]+$) || ("${SONATYPE_RELEASE_VERSION}" =~ ^[0-9]+\.[0-9]+\.[0-9]+-rc[0-9]+$) ]]; then
     REMOTE_REPOSITORY="sonatype-twilio::::https://oss.sonatype.org/content/repositories/$SONATYPE_REPO_ID"
+    ARTIFACT="com.twilio:${SONATYPE_ARTIFACT_ID}:${SONATYPE_RELEASE_VERSION}:jar:dokka"
+    ARTIFACT_FILENAME="$SONATYPE_ARTIFACT_ID-$SONATYPE_RELEASE_VERSION-dokka.jar"
   else
     echo "Version ${SONATYPE_RELEASE_VERSION} is not Release or Release Candidate" >&2  # write error message to stderr
     exit 1
   fi
-  echo "==== Fetching package $SONATYPE_ARTIFACT_ID version $SONATYPE_RELEASE_VERSION from Sonatype $REMOTE_REPOSITORY to $RESULT_ARTIFACTS_DIR ===="
+  echo "==== Fetching package $SONATYPE_ARTIFACT_ID version $SONATYPE_RELEASE_VERSION from Sonatype $REMOTE_REPOSITORY ===="
 
-  mvn dependency:get -settings sonatype-settings.xml -DremoteRepositories=$REMOTE_REPOSITORY  -Dartifact=com.twilio:${SONATYPE_ARTIFACT_ID}:${SONATYPE_RELEASE_VERSION}:jar:dokka -Dtransitive=false -Ddest="$RESULT_ARTIFACTS_DIR/sdk/build/outputs/lib/sdk-dokka.jar" || exit 1
-
+  mvn dependency:get -settings sonatype-settings.xml -DremoteRepositories="$REMOTE_REPOSITORY" -Dartifact="$ARTIFACT" -Dtransitive=false || exit 1
+  
   mkdir -p "$RESULT_ARTIFACTS_DIR/sdk/build/docs/dokka/$SONATYPE_ARTIFACT_ID"
-  unzip "$RESULT_ARTIFACTS_DIR/sdk/build/outputs/lib/sdk-dokka.jar" -d "$RESULT_ARTIFACTS_DIR/sdk/build/docs/dokka/$SONATYPE_ARTIFACT_ID"
+  unzip "$HOME/.m2/repository/com/twilio/$SONATYPE_ARTIFACT_ID/$SONATYPE_RELEASE_VERSION/$ARTIFACT_FILENAME" -d "$RESULT_ARTIFACTS_DIR/sdk/build/docs/dokka/$SONATYPE_ARTIFACT_ID" || exit 1
 }
